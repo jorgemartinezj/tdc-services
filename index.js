f1f8f31c4a01564ff8354eaafd8f0dedb1e14b69
Jorge Martinez Jimenez <jorge.martinezj@hotmail.com>
2024-05-28 18:00:17 -0600

    Agregar workflow para despligue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PUERTO = 3000;
+const PUERTO = process.env.PORT || 3000;
 
 app.listen(PUERTO, () => {
   console.log(`Servicio en linea sobre el puerto ${PUERTO}`);
